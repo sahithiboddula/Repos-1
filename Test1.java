210145f7b4f483e9daa110fc1f89a5fdc85f1165
sahithiboddula <sahithiboddula2@gmail.com>
2025-10-26 16:59:54 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,18 @@
+class Test1
+{
+	void m1()
+	{
+		System.out.println(String[] args);
+	}
+	static void m2()
+	{
+		System.out.println("this is static method");
+	}
+	public static void main(String[] args) 
+	{
+		Test1 t = new Test();
+		t.m1();
+		Test1.m2();
+	}
+}
+
